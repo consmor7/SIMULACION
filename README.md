9fe590631797f066bc5f86ab81f70884fccdc450
Angel Constantino <69411099+consmor7@users.noreply.github.com>
2023-03-14 10:59:54 -0600

    git commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SIMULACION
 Task 
-- Prueba de arriba y abajo
+- Prueba de corridas arriba y abajo
+- Prueba de Kolmogorov Smirnov
+- Prueba de Poker 
--- src/simulacion/kolmogorov.java
+++ src/simulacion/kolmogorov.java
@@ -4,13 +4,42 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class kolmogorov {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         //Se leen los numeros de un archivo txt y se guardan en un arreglo
         float[]array= readFiles("C:/Users/Angel/Documents/numeros15.txt");
         System.out.println(Arrays.toString(array));
-        
+
+        int n = array.length;
+
+        //Ordenas ascendentemente los números, usamos el método de la burbuja
+        BubbleSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]+ ""); 
+        }
+  
     }
+   
+    private static void BubbleSort(float[] array) {
+        int n =  array.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (array[j]>array[j+1]) {
+                    int temp = (int) array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+  
+                }
+                
+            }
+            
+        }
+    }
+
     public static float[] readFiles(String file){
         try{
             File f = new File(file);
