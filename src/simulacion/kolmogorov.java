c0329430d4605c97834269119b95391b4c2b011e
Angel Constantino <69411099+consmor7@users.noreply.github.com>
2023-03-15 19:09:08 -0600

    kolmogorov

--- src/simulacion/kolmogorov.java
+++ src/simulacion/kolmogorov.java
@@ -1,32 +1,50 @@
 package simulacion;
+import java.lang.Math;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class kolmogorov {
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
+    
+    /*La siguiente prueba de Kolmogorov Smirnov 
+    se puede aplicar a muestras de 10 numeros pseudoaleatorios
+    si se desea ampliar esta posibilidad,es necesario cambiar los parametros*/
 
-         /*Aqui se ingresan los números mediante un archivo txt 
+    public static void main(String[] args) {
+        
+        /*Aqui se ingresan los números mediante un archivo txt 
         y estos son guardados en un arreglo.*/
   
-        float[]array= readFiles("C:/Users/Angel/Documents/numeros15.txt");
+        float[]array= readFiles("C:/Users/Angel/Documents/GitHub/SIMULACION/números aleatorios/muestras(10)/10.txt");
+        System.out.println("NUMEROS INGRESADOS:");
         System.out.println(Arrays.toString(array));
         
         int size = 10; //Variable que especifica el tamaño del arreglo
         
-        //Aplicamos uniformidad al arreglo.
-        Uniformidad(array);
+        //aplicamos uniformidad al arreglo.
+        for (int i = 0; i < size; i++) {
+            array[i]= array[i]/256 ;
+        }
+         System.out.println("NUMEROS CON UNIFORMIDAD :");
+         System.out.println(Arrays.toString(array));
         
         // Acumulada observada
         //Orden de los numeros, usando metodo de la burbuja 
-        BubbleSort(array);
-
-        //HASTA AQUI LOS N NUMEROS SE ENCUENTRAN UNIFORMEMENTE DISTRIBUIDOS (0,1) Y ORDENADOS ACENDENTEMENTE. 
+        float aux=0; //Variable auxiliar  
+        for (int j = 0; j < size; j++) {
+            for (int i = 0; i < size-1; i++) {
+                if (array[i]>array[i+1]) {
+                    aux=array[i];
+                    array[i]=array[i+1];
+                    array[i+1]=aux;
+                } 
+            }
+        }
+         System.out.println("NUMEROS ORDENADOS ASCENDENTEMENTE:");
+         System.out.println(Arrays.toString(array));
+        /*HASTA AQUI LOS N NUMEROS SE ENCUENTRAN UNIFORMEMENTE DISTRIBUIDOS (0,1)
+         Y ORDENADOS ACENDENTEMENTE. */
        
         //Acumulada especifica 
         float numeros [] ={1,2,3,4,5,6,7,8,9,10};
@@ -35,18 +53,22 @@ public class kolmogorov {
         for (int i = 0; i < 10; i++) {
             array_especifica[i]= numeros[i]/10;
             
-        }       
+        }  
+         System.out.println("ACUMULADA ESPECIFICA:");
+         System.out.println(Arrays.toString(array_especifica));
        
-        //Declaracion de arreglo para la absoluta o diferencia
+        //Declaracion de arreglo para la Dif absoluta 
 
         float array_absoluta []= new float[size];
         
         for (int i = 0; i < size; i++) {
             array_absoluta[i]= Math.abs(array[i]-array_especifica[i]);
-            System.out.println(array_absoluta[i]);
+            
         }
+         System.out.println("DIFERENCIA ABSOLUTA :");
+         System.out.println(Arrays.toString(array_absoluta));
 
-        //Prueba para visualizar D maxima 
+        //Prueba para encontrar D maxima 
         float dmax =0;
         for (int x = 0; x < array_absoluta.length; x++) {
             if (array_absoluta[x]>dmax) {
@@ -58,63 +80,40 @@ public class kolmogorov {
         float dcritico = (float) 0.409;
         if (dmax<dcritico) {
             System.out.println("No se rechaza Ho");
-            System.out.println("Ho: Los numeros generados estan U(0,1) con un marco de error de 0.5");
+            System.out.println("Ho: Los numeros generados estan U(0,1) con un marco de error de 0.05");
             
         }else{
             System.out.println("Se rechara Ho");
             System.out.println("Ho: Los numeros generados no estan U(0,1), por lo tanto no pueden usarsse");
         }
-       
+                   
+    } //Clase main acaba aqui
         
-
-        
-  
-    }
-    private static void Uniformidad(float[] array) {
-        int n = array.length;
-        for (int i = 0; i < n; i++) {
-            array[i]= array[i]/256;
-        }
-        
-    }
-   
-    private static void BubbleSort(float[] array) {
-        int n =  array.length;
-        for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j < n-i-1; j++) {
-                if (array[j]>array[j+1]) {
-                    int temp = (int) array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
-  
-                }
-                
-            }
-            
-        }
-    }
-
+        /*Este método nos ayuda a leer los numeros que se 
+        encuentran en un archivo con extensión txt y agrega los numeros 
+        a un arreglo del tamaño del total de la muestra 
+         */
     public static float[] readFiles(String file){
-        try{
-            File f = new File(file);
-            Scanner s = new Scanner(f);
-            int ctr = 0;
-            while (s.hasNextDouble()){
-                ctr++;
-                s.nextDouble();
-            }
-            float[] arr = new float[ctr];
-            
-            Scanner s1 = new Scanner(f);
-            
-            for (int i = 0; i < arr.length; i++) 
-                arr[i]= s1.nextFloat();
-            return arr;
-                
-        }
-        catch(FileNotFoundException e){
-            return null;
-        }
-        
-     }
+       try{
+           File f = new File(file);
+           Scanner s = new Scanner(f);
+           int ctr = 0;
+           while (s.hasNextDouble()){
+               ctr++;
+               s.nextDouble();
+           }
+           float[] arr = new float[ctr];
+           
+           Scanner s1 = new Scanner(f);
+           
+           for (int i = 0; i < arr.length; i++) 
+               arr[i]= s1.nextFloat();
+           return arr;
+               
+       } //Si el archivo txt se encuentra vacío,lanza una excepción. 
+       catch(FileNotFoundException e){
+           return null;
+       }
+       
+    }
 }
